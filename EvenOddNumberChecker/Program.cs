42040083648ab8336182598208ab0327db098d31
Ghidyon <gideonakunana@gmail.com>
2021-07-06 05:28:47 +0200

    initial build

---
+++ EvenOddNumberChecker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EvenOddNumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
+            int inputNumber = Convert.ToInt32(input);
+
+            if (inputNumber % 2 != 0)
+            {
+                Console.WriteLine($"{inputNumber} is an odd number");
+            }
+            else
+            {
+                Console.WriteLine($"{inputNumber} is an even number");
+            }
+        }
+    }
+}
